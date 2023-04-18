10a8816e2c6f536f1d65f447427147ed95bc4ca1
Facundo Alvarez <falvarez@casino>
2023-04-18 16:04:23 -0300

    Se agrega todo el programa casi completo

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world y hola Intento de Spotify");
